bbc7fedcf9cc03a9ac912e16d2a757c4aa2889d9
Alex Bird <bird@stridehealth.com>
2018-03-07 17:54:20 -0800

    allow filtering by group and system name w/regexp; remove metrics after each papertrail scrape

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/alexebird/papertrail-exporter/papertrail"
@@ -13,29 +14,42 @@ import (
 )
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-
-var lastEventAt = prometheus.NewGaugeVec(
-	prometheus.GaugeOpts{
-		Namespace: "papertrail",
-		Subsystem: "system",
-		Name:      "last_event_at_seconds",
-		Help:      "Last event seen at for the system.",
-	},
-	[]string{"group", "name"},
-)
+var groupFitler = flag.String("group-filter", ".*", "A regex by which to whitelist group names.")
+var systemFitler = flag.String("system-filter", ".*", "A regex by which to whitelist system names.")
 
 func main() {
 	flag.Parse()
-
-	prometheus.MustRegister(lastEventAt)
+	log.SetLevel(log.DebugLevel)
+	groupFilterRegex := regexp.MustCompile(*groupFitler)
+	systemFilterRegex := regexp.MustCompile(*systemFitler)
+	var lastEventAt *prometheus.GaugeVec
 
 	go func() {
 		for {
 			groups := papertrail.ListGroups()
 
+			if lastEventAt != nil {
+				prometheus.Unregister(lastEventAt)
+			}
+
+			lastEventAt = prometheus.NewGaugeVec(
+				prometheus.GaugeOpts{
+					Namespace: "papertrail",
+					Subsystem: "system",
+					Name:      "last_event_at_seconds",
+					Help:      "Last event seen at for the system.",
+				},
+				[]string{"group", "name"},
+			)
+			// re-register so that old systems are purged from the metrics
+			prometheus.MustRegister(lastEventAt)
+
 			for _, group := range groups {
 				for _, system := range group.Systems {
-					lastEventAt.WithLabelValues(group.Name, system.Name).Set(float64(system.LastEventAt.Unix()))
+					if groupFilterRegex.MatchString(group.Name) && systemFilterRegex.MatchString(system.Name) {
+						log.Debug(group.Name + " " + system.Name)
+						lastEventAt.WithLabelValues(group.Name, system.Name).Set(float64(system.LastEventAt.Unix()))
+					}
 				}
 			}
 
